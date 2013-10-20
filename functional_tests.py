b7499d03c58a47c9c5d6cd2f46de9053cd3ecad9
Rob Graziano <robgraziano@earthlink.net>
2013-10-19 20:50:09 -0400

    Refactor home page view to use templates

--- functional_tests.py
+++ functional_tests.py
@@ -16,21 +16,36 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # She is invited to neter a to-do item straight away
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_tag_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         #She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item on a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy Peacock Feathers' for row in rows)
+        )
 
         # There is still a text box inviting her ot add another item. She
         # enters "Use peacock feathers to make a fly" (Edisth is very methodical)
+        self.fail('Finish the test!')
 
-        # The page updates again, and now shows boht items on her list
+        # The page updates again, and now shows both items on her list
 
         #Edit wonders whetehr the site will remember he list. Then she sees
         # that the site has generated a unique URL for her -- there is som
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-# Create your views here.
+from django.shortcuts import render
 
-def home_page():
-    pass
+def home_page(request):
+    return render(request, 'home.html')
