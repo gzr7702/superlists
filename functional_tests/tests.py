4534740b7a8a3fcf9e8c4232ae7efecbf5ca005b
Rob Graziano <robgraziano@earthlink.net>
2013-11-13 18:57:42 -0500

    "move functional_test to it's own app"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -55,7 +55,7 @@ class NewVisitorTest(unittest.TestCase):
             [row.text for row in rows]
         )
 
-        #Edit wonders whetehr the site will remember he list. Then she sees
+        #Edit wonders whether the site will remember he list. Then she sees
         # that the site has generated a unique URL for her -- there is som
         # explanatory text to that effect.
         self.fail('Finish the test!')
@@ -64,5 +64,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to cleep
 
-if __name__ == "__main__":
-    unittest.main()
